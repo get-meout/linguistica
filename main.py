e7eb67519604d6dac530f28d87259cda5678776e
Zimo Luo <zimoluo@192-168-1-108.tpgi.com.au>
2024-03-29 21:31:27 +1100

    added play again feature

--- main.py
+++ main.py
@@ -13,29 +13,37 @@ def get_random_word():
         words = file.read().splitlines()
         return random.choice(words)
 
-print_menu()
-final_word = get_random_word()
+# print_menu()
+# final_word = get_random_word()
+play_again = ""
+while play_again != "q":
 
-# print(final_word) # debugging REMOVE LATER
+    print_menu()
+    final_word = get_random_word()
+    print(final_word) # debugging REMOVE LATER
 
-for attempt in range(0, 6):
-    guess = input().lower()
+    for attempt in range(0, 6):
+        guess = input().lower()
 
-    if len(guess) != 5:
-        while (guess := input().lower()):
-            continue
+        sus.stdout.write('\x1b[1A') # moves cursor up one line
+        sus.stdout.write('\x1b[2K') # clears uncoloured user's guess
 
-    for i in range( min(len(guess), 5) ):
-        if guess[i] == final_word[i]: 
-            print(colored(guess[i], 'green'), end = "") 
-        elif guess[i] in final_word:
-            print(colored(guess[i], 'yellow'), end = "") 
-        else:
-            print(colored(guess[i], 'grey'), end = "") 
+        if len(guess) != 5:
+            while (guess := input().lower()):
+                continue
 
-        if i == 4:
-            print()
+        for i in range( min(len(guess), 5) ):
+            if guess[i] == final_word[i]: 
+                print(colored(guess[i], 'green'), end = "") 
+            elif guess[i] in final_word:
+                print(colored(guess[i], 'yellow'), end = "") 
+            else:
+                print(colored(guess[i], 'grey'), end = "") 
 
-    if guess == final_word:
-        print(colored(f"Congrats! You got solved today's linguistica in {attempt+1}", 'red'))
-        break
+        print()
+
+        if guess == final_word:
+            print(colored(f"Congrats! You got solved today's linguistica in {attempt+1}", 'red'))
+            break
+        
+    play_again = input("Play again? if not type q to quit: ")
