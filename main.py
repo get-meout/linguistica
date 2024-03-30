5d5420f9fb3afd0a2a34fce9caee4364a902a4d7
Zimo Luo <zimoluo@192-168-1-108.tpgi.com.au>
2024-03-30 12:14:53 +1100

    did error checking, needs to be 5 letters, and valid word

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ import random
 import sys as sus
 from termcolor import colored 
 # everytime i do import i need to do 'pip install <import_name>'
-import nltk 
-from nltk.corpus import words 
+# import nltk
+# nltk.download('words')
+# import nltk 
+# from nltk.corpus import words 
 
 def print_menu():
     print("Let's play Linguistica: ")
@@ -17,9 +19,9 @@ def get_random_word():
         words = file.read().splitlines()
         return random.choice(words)
 
-nltk.data.path.append('/work/words')
-word_list = words.words()
-words_five = [word for word in word_list if len(word) == 5]
+# nltk.data.path.append('/work/words')
+# word_list = words.words()
+# words_five = [word for word in word_list if len(word) == 5]
 
 play_again = ""
 while play_again != "q":
@@ -35,8 +37,17 @@ while play_again != "q":
         sus.stdout.write('\x1b[2K') # clears uncoloured user's guess
 
         if len(guess) != 5:
+            print(f"{guess} is not a 5 letter word, try again")
             while (guess := input().lower()):
                 continue
+        
+        with open('valid_words.txt', 'r') as valid_words_file:
+            contents = valid_words_file.read()
+            if str(guess) not in contents:
+                print(f"{guess} is not in the linguistica dictionary, try again")
+                continue
+
+        valid_words_file.close()
 
         for i in range( min(len(guess), 5) ):
             if guess[i] == final_word[i]: 
